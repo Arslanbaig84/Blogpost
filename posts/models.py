0028808009571f68a8dfe065057a3901ced5ab45
Arslanbaig84 <arslan.multiline@gmail.com>
2024-10-06 20:44:30 +0500

    first commit

---
+++ posts/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+from base.models import BaseModel
+from django.utils.text import slugify
+
+
+# Create your models here.
+class Post(BaseModel):
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(unique=True)
+    article = models.TextField(max_length=2000)
+    
+    def __str__(self) -> str:
+        return self.title
+    
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.title)
+        super(Post, self).save(*args, **kwargs)
