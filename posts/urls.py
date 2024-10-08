86d22195e54a58867accba7946e5737d0febcb29
Arslanbaig84 <arslan.multiline@gmail.com>
2024-10-08 06:03:36 +0500

    message added for new post

--- posts/urls.py
+++ posts/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [ 
-    path('', views.index, name='index'),
+    path('', views.posts, name='posts'),
     path('new_post/', views.new_post, name='new_post'),
     path('<slug:slug>/', views.post, name='post'),
 ]
