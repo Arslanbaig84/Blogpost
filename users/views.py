4d1f30d3c0e6a75ae59892389ec91e5744a6653a
Arslanbaig84 <arslan.multiline@gmail.com>
2024-10-10 17:01:36 +0500

    log out route

--- users/views.py
+++ users/views.py
@@ -1,10 +1,17 @@
 from django.shortcuts import render, redirect
 from .forms import CustomUserCreationForm
-
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
+from django.contrib import messages
 
 # Create your views here.
 def register(request):
     if request.method == 'POST':
+        email = request.POST.get('email')
+        if User.objects.filter(email = email).exists():
+            messages.info(request, 'User already exists with this email.')
+            return redirect('register')
+        
         form = CustomUserCreationForm(request.POST)
         if form.is_valid():
             user = form.save()
@@ -15,4 +22,20 @@ def register(request):
 
 
 def login_user(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user:
+            login(request, user)
+            messages.success(request, "Login Successful.")
+            #To Do
+            return redirect('/')
+
     return render(request, 'users/login_user.html')
+
+
+def logout_user(request):
+    logout(request)
+    messages.info(request, 'Logged Out')
+    return redirect('login_user')
