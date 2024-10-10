ef1c7a1924d874dff0996713ded2405466e282e6
Arslanbaig84 <arslan.multiline@gmail.com>
2024-10-10 16:30:00 +0500

    user registration complete

--- users/views.py
+++ users/views.py
@@ -4,5 +4,11 @@ from .forms import CustomUserCreationForm
 
 # Create your views here.
 def register(request):
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            # To Do
+            return redirect('/')
     form = CustomUserCreationForm
     return render(request, 'users/register.html', {'form':form})
