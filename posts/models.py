c881c8cc12df5c07d1e0d8d347b4497522e86e47
Arslanbaig84 <arslan.multiline@gmail.com>
2024-10-12 05:33:46 +0500

    author added to model

--- posts/models.py
+++ posts/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from base.models import BaseModel
 from django.utils.text import slugify
+from django.contrib.auth.models import User
 
 
 # Create your models here.
@@ -8,6 +9,7 @@ class Post(BaseModel):
     title = models.CharField(max_length=200)
     slug = models.SlugField(unique=True, null=True, blank=True)
     article = models.TextField(max_length=2000)
+    author = models.ForeignKey(User,on_delete=models.CASCADE, related_name='post')
     
     def __str__(self) -> str:
         return self.title
